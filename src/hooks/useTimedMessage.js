936a1bbebfae5abd44c25d2b02504ee61949f755
Arthur Hauser <arthur.hauser@gmail.com>
2025-03-29 17:28:20 -0400

    initial commit

---
+++ src/hooks/useTimedMessage.js
@@ -0,0 +1,9 @@
+import { useState, useEffect } from "react";
+
+/** Where is this component in the source files?
+ */
+
+function useTimedMessage() {
+  console.log("where is the useTimeMessage function?");
+}
+export default useTimedMessage;
